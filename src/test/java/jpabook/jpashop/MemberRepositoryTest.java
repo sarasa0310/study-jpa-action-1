ffc0189dcede2ca1488f31c6996d7fb74768f109
sarasa0310 <dev.sarasa.0310@gmail.com>
2023-07-26 15:37:33 +0900

    회원 도메인 개발 완료
    - 웹 계층을 제외한 Service, Repository 계층 개발 완료
    - 비즈니스 로직 테스트 추가
    - 기존 레포지토리 테스트 업데이트
    - application.yml 파일을 test 디렉토리에도 추가하여 테스트용 db를 인메모리로 분기시킴

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -1,7 +1,7 @@
 package jpabook.jpashop;
 
 import jpabook.jpashop.domain.member.Member;
-import jpabook.jpashop.domain.member.MemberRepository;
+import jpabook.jpashop.repository.MemberRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -23,11 +23,11 @@ class MemberRepositoryTest {
         member.setName("memberA");
 
         // when
-        Long savedId = memberRepository.save(member);
-        Member findMember = memberRepository.find(1L);
+        memberRepository.save(member);
+        Member findMember = memberRepository.findOne(member.getId());
 
         // then
-        assertThat(findMember.getId()).isEqualTo(savedId);
+        assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getName()).isEqualTo(member.getName());
         assertThat(member).isEqualTo(findMember);
     }
---
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -0,0 +1,55 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.member.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Transactional
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired
+    private MemberService memberService;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("회원가입")
+    void joinTest() {
+        // given
+        Member member = new Member();
+        member.setName("kim");
+
+        // when
+        Long savedId = memberService.join(member);
+
+        // then
+        assertThat(member).isEqualTo(memberRepository.findOne(savedId));
+    }
+
+    @Test
+    @DisplayName("같은 이름의 회원이 이미 존재할 경우 예외가 발생해야 한다.")
+    void validateDuplicateMemberTest() {
+        // given
+        Member member1 = new Member();
+        member1.setName("jimmy");
+
+        memberService.join(member1);
+
+        Member member2 = new Member();
+        member2.setName("jimmy");
+
+        // when & then
+        assertThatThrownBy(() -> memberService.join(member2))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("이미 존재하는 회원입니다.");
+    }
+
+}
