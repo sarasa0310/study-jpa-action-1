f565db974d765737b9991a0d10df32af2e6ee1b4
sarasa0310 <dev.sarasa.0310@gmail.com>
2023-08-23 12:53:58 +0900

    QueryDSL 소개 학습
    - 동적인 쿼리를 쉽게 생성할 수 있는 QueryDSL
    - build.gradle QueryDSL 환경 설정
    - 기존 복잡한 쿼리 메서드 대신 단순하게 만들 수 있는 방법 학습
    - LazyInitializationException 때문에 jpa.open-in-view true 설정

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop.repository;
 
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jpabook.jpashop.domain.order.Order;
-import lombok.RequiredArgsConstructor;
+import jpabook.jpashop.domain.order.OrderStatus;
 import org.springframework.stereotype.Repository;
 import org.springframework.util.StringUtils;
 
@@ -9,11 +11,19 @@ import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
+import static jpabook.jpashop.domain.member.QMember.member;
+import static jpabook.jpashop.domain.order.QOrder.order;
+
 @Repository
-@RequiredArgsConstructor
 public class OrderRepository {
 
     private final EntityManager em;
+    private final JPAQueryFactory query;
+
+    public OrderRepository(EntityManager em) {
+        this.em = em;
+        this.query = new JPAQueryFactory(em);
+    }
 
     public void save(Order order) {
         em.persist(order);
@@ -57,6 +67,34 @@ public class OrderRepository {
             query = query.setParameter("name", orderSearch.getMemberName());
         }
         return query.getResultList();
+    }
+
+    public List<Order> findAll(OrderSearch orderSearch) {
+//        JPAQueryFactory query = new JPAQueryFactory(em);
+//        QOrder order = QOrder.order;
+//        QMember member = QMember.member;
+
+        return query
+                .select(order)
+                .from(order)
+                .join(order.member, member)
+                .where(statusEq(orderSearch.getOrderStatus()), nameLike(orderSearch.getMemberName()))
+                .limit(1000)
+                .fetch();
+    }
+
+    private BooleanExpression nameLike(String memberName) {
+        if (!StringUtils.hasText(memberName)) {
+            return null;
+        }
+        return member.name.like(memberName);
+    }
+
+    private BooleanExpression statusEq(OrderStatus status) {
+        if (status == null) {
+            return null;
+        }
+        return order.status.eq(status);
     }
 
     public List<Order> findAllWithMemberDelivery() {
--- src/main/java/jpabook/jpashop/service/OrderService.java
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -55,7 +55,7 @@ public class OrderService {
 
     // 검색
     public List<Order> findOrders(OrderSearch orderSearch) {
-        return orderRepository.findAllByString(orderSearch);
+        return orderRepository.findAll(orderSearch);
     }
 
 }
